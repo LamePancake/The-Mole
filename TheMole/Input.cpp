872a29bc29cfdaa23a8f822ebb58225c024656d3
mattku <mattkuu@gmail.com>
2016-02-03 14:40:52 -0800

    Framework for interpreting input from Keyboard and Gamepad

--- TheMole/Input.cpp
+++ TheMole/Input.cpp
@@ -3,6 +3,8 @@
 
 Input::Input()
 {
+	curKeyboardState = SDL_GetKeyboardState(NULL);
+	prevKeyboardState = curKeyboardState;
 }
 
 
@@ -10,20 +12,49 @@ Input::~Input()
 {
 }
 
-void Input::Poll_Event()
+void Input::UpdateKeyboardState() {
+	prevKeyboardState = curKeyboardState;
+	curKeyboardState = SDL_GetKeyboardState(NULL);
+}
+
+void Input::PollEvent()
 {
-	while (SDL_PollEvent(&event))
+	while (SDL_PollEvent(&generalEvent))
 	{
-		switch (event.type)
+		switch (generalEvent.type)
 		{
 		case SDL_KEYDOWN:
 			//Doesn't do anything at this point
 		case SDL_KEYUP:
-			On_Key_Up(event.key.keysym);
+			On_Key_Up(generalEvent.key.keysym);
 		default:
 			break;
 		}
 	}
+}
+
+//The parameter value should be an enumerated value that matches
+//SDL_Scancode values
+bool Input::KeyPressed(SDL_Scancode key)
+{
+	if (key == NULL)
+		return false;
+	if (prevKeyboardState[key] && !curKeyboardState[key])
+		return true;
+	else
+		return false;
+}
+
+//The parameter value should be an enumerated value that matches
+//SDL_Scancode values
+bool Input::KeyHeld(SDL_Scancode key)
+{
+	if (key == NULL)
+		return false;
+	if (prevKeyboardState[key] && curKeyboardState[key])
+		return true;
+	else
+		return false;
 }
 
 void Input::On_Key_Up(SDL_Keysym keysym)
@@ -46,5 +77,3 @@ void Input::On_Key_Up(SDL_Keysym keysym)
 		break;
 	}
 }
-
-
